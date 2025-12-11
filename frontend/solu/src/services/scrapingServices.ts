4e106ecdc385414f3e5c54bf2c62140ae538655a
moremoney32 <francklionelngongangtchouta@gmail.com>
2025-12-11 14:47:33 +0100

    changeurl

--- frontend/solu/src/services/scrapingServices.ts
+++ frontend/solu/src/services/scrapingServices.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 import type { ScrapingFilters, EntrepriseScraped } from '../types/scraping.type';
 import { mockRegions, mockVilles, mockActivites } from '../data/scrapingData';
 
- const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
- //const API_BASE_URL = 'https://solutravo-scraping.elyfthosting.tech/api';
+ //const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+ const API_BASE_URL = 'https://solutravo-scraping.elyfthosting.tech/api';
  //const API_BASE_URL = 'http://46.202.130.175:3002/api';
 
 
